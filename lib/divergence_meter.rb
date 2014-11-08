3e10452d30c9c4fb42caba50e506d4e9f0a9168b
sugamasao <sugamasao@gmail.com>
2014-11-09 00:55:34 +0900

    引数チェックを行うようにした

--- lib/divergence_meter.rb
+++ lib/divergence_meter.rb
@@ -1,5 +1,6 @@
 require 'divergence_meter/version'
 require_relative 'divergence_meter/levenshtein_distance'
+require_relative 'divergence_meter/cli'
 
 module DivergenceMeter
   def self.run(str1, str2)
---
+++ lib/divergence_meter/cli.rb
@@ -0,0 +1,35 @@
+module DivergenceMeter
+  class CLI
+    def initialize(argv)
+      @argv = argv
+    end
+
+    def parse
+      if @argv.include?('-h') || @argv.include?('--help')
+        puts usage
+        exit
+      end
+
+      if @argv.include?('-v') || @argv.include?('--version')
+        puts version
+        exit
+      end
+
+      unless @argv.size == 2
+        puts 'Invalid Argument'
+        puts usage
+        exit 1
+      end
+    end
+
+    private
+
+    def usage
+      "usage: #{ $0 } word1 word2"
+    end
+
+    def version
+      "Version: #{ VERSION }"
+    end
+  end
+end
